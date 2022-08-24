025a3ec4a6f512050c8b79990d5840318a6d215d
franpersanchez <franpersanchez@gmail.com>
2022-08-24 17:00:39 +0200

    prueba para scheduler de heroku

--- FileGenerator.py
+++ FileGenerator.py
@@ -2,45 +2,45 @@ import requests,xmltodict,json,os
 
 
     
-def fileCreation():
-    urls=[
-    'https://ec.europa.eu/food/plant/pesticides/eu-pesticides-database/mrls/?event=mrl.xml&_=1',
-    'https://ec.europa.eu/food/plant/pesticides/eu-pesticides-database/mrls/?event=mrl.xml&_=2',
-    'https://ec.europa.eu/food/plant/pesticides/eu-pesticides-database/mrls/?event=mrl.xml&_=3',
-    'https://ec.europa.eu/food/plant/pesticides/eu-pesticides-database/mrls/?event=mrl.xml&_=4',
-    'https://ec.europa.eu/food/plant/pesticides/eu-pesticides-database/mrls/?event=mrl.xml&_=5',
-    'https://ec.europa.eu/food/plant/pesticides/eu-pesticides-database/mrls/?event=mrl.xml&_=6',
-    'https://ec.europa.eu/food/plant/pesticides/eu-pesticides-database/mrls/?event=mrl.xml&_=7',
-    'https://ec.europa.eu/food/plant/pesticides/eu-pesticides-database/mrls/?event=mrl.xml&_=8'
-    ]
 
-    files=[]
+urls=[
+'https://ec.europa.eu/food/plant/pesticides/eu-pesticides-database/mrls/?event=mrl.xml&_=1',
+'https://ec.europa.eu/food/plant/pesticides/eu-pesticides-database/mrls/?event=mrl.xml&_=2',
+'https://ec.europa.eu/food/plant/pesticides/eu-pesticides-database/mrls/?event=mrl.xml&_=3',
+'https://ec.europa.eu/food/plant/pesticides/eu-pesticides-database/mrls/?event=mrl.xml&_=4',
+'https://ec.europa.eu/food/plant/pesticides/eu-pesticides-database/mrls/?event=mrl.xml&_=5',
+'https://ec.europa.eu/food/plant/pesticides/eu-pesticides-database/mrls/?event=mrl.xml&_=6',
+'https://ec.europa.eu/food/plant/pesticides/eu-pesticides-database/mrls/?event=mrl.xml&_=7',
+'https://ec.europa.eu/food/plant/pesticides/eu-pesticides-database/mrls/?event=mrl.xml&_=8'
+]
 
-    for index,url in enumerate(urls):
+files=[]
 
-    #create response object
-        r=requests.get(url)
-        #we iterate through the list to give a name to each downloaded xml file
-        filename = "MRL" + str(index)+ ".xml"    
-        
-        with open(filename,'wb') as file:
-    #write a file with each download
-            file.write(r.content)
-            file.close()
+for index,url in enumerate(urls):
 
-            if file.closed:
-        # open the input xml file and read data 
-                with open(filename, errors='ignore') as xml_file: 
-                    
-                    my_dict = xmltodict.parse(xml_file.read())
+#create response object
+    r=requests.get(url)
+    #we iterate through the list to give a name to each downloaded xml file
+    filename = "MRL" + str(index)+ ".xml"    
+    
+    with open(filename,'wb') as file:
+#write a file with each download
+        file.write(r.content)
+        file.close()
 
-                    xml_file.close() 
-                    os.remove(filename)
-                    
-                    json_data = json.dumps(my_dict)
-                    
-                    # write the json data to output  
-                    jsonfile="MRL"+str(index)+ ".json"
-                    with open(jsonfile, "w") as json_file: 
-                        json_file.write(json_data) 
-                        json_file.close() 
+        if file.closed:
+    # open the input xml file and read data 
+            with open(filename, errors='ignore') as xml_file: 
+                
+                my_dict = xmltodict.parse(xml_file.read())
+
+                xml_file.close() 
+                os.remove(filename)
+                
+                json_data = json.dumps(my_dict)
+                
+                # write the json data to output  
+                jsonfile="MRL"+str(index)+ ".json"
+                with open(jsonfile, "w") as json_file: 
+                    json_file.write(json_data) 
+                    json_file.close() 
